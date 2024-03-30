f1b94f5ac0b63c00e3441148b12489626e330760
Dwayne Redwoodm <dwayneredwood@live.com>
2024-03-30 04:12:08 -0500

    added DotNetEnv nuget package

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DotNetEnv;
 using Azure.Messaging.ServiceBus;
+
+namespace azure_servicebus_queue_devcontainer
+{
+    class Program
+    {
+        // connection string to your Service Bus namespace
+        string connectionString = "CONNECTION_STRING";
+
+        // name of your Service Bus topic
+        string queueName = "QUEUE_NAME";
+        
+        static void Main(string[] args){
+            DotNetEnv.Env.Load();
+            Console.WriteLine("DotNetEnv Test");
+            Console.WriteLine(Program.connectionString);
+            // Console.WriteLine(Environment.GetEnvironmentVariable(connectionString));
+        } 
+    }
+}
